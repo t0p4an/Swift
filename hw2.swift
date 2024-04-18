338bcf240daf87657a3895446d835f6fa708610c
t0p4an <148556550+t0p4an@users.noreply.github.com>
2024-04-18 18:18:24 +0300

    Add files via upload

---
+++ hw2.swift
@@ -0,0 +1,25 @@
+typealias pipi = Bool
+typealias popo = Int
+typealias dd = Double
+
+// Площадь квадрата
+let aq: popo = 5
+let sq = aq * aq
+print("Площадь квадрата равна \(sq)")
+
+//Площадь треугольника
+let at: dd = 10.0
+let bt: dd = 10.0
+let ct: dd = 15.0
+let st = 0.5 * at * bt
+print("Площадь треугольника равна \(st)")
+
+//Площадь трапеции
+let ar: dd = 2.0
+let br: dd = 4.0
+let hr: dd = 6.0
+let sr: dd = 0.5 * (ar + br) * hr
+print("Площадь трапеции равна \(sr)")
+
+
+
---
+++ hw3.swift
@@ -0,0 +1,27 @@
+var a = ""
+var b = ""
+var c = " "
+a = "Здарова"
+b = "Заебал"
+print(a + c + b)
+
+for d in "Пельмень"{
+    print(d)
+}
+
+var perempt = ""
+if perempt.isEmpty{
+    print("Пустая")
+} else {
+    print("Не пустая")
+}
+
+var perno = "k"
+if perno.isEmpty{
+    print("Пустая")
+} else {
+    print("Не пустая")
+}
+
+let cou = a.count
+print(cou)
