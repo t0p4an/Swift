f5bf6046f56637e5875225a878078e7f8bc8ddd9
t0p4an <148556550+t0p4an@users.noreply.github.com>
2024-04-19 14:08:24 +0300

    Update hw2.swift

--- hw2.swift
+++ hw2.swift
@@ -1,3 +1,4 @@
+import SwiftUI
 typealias pipi = Bool
 typealias popo = Int
 typealias dd = Double
@@ -20,6 +21,18 @@ let br: dd = 4.0
 let hr: dd = 6.0
 let sr: dd = 0.5 * (ar + br) * hr
 print("Площадь трапеции равна \(sr)")
+
+//Квадратное уравнение x^2 - 2x - 15 = 0
+let a = Double(1)
+let b = Double(-2)
+let c = Double(-15)
+
+let d = Double(b * b - 4 * a * c)
+let sqrtd = sqrt(d)
+let x1 = (-b + sqrtd) / (2 * a)
+let x2 = (-b - sqrtd) / (2 * a)
+print("x1 = \(x1)")
+print("x2 = \(x2)")
 
 
 
